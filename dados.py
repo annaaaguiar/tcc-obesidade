9e9c7520be4bd428c2941d6dd45cbdbce6fc31db
annaaaguiar <annaaguiar.galo@gmail.com>
2025-10-03 14:08:03 -0300

    Update dados.py

--- dados.py
+++ dados.py
@@ -282,7 +282,7 @@ with tab2:
         plotar_associacao(df_filtrado, 'obesidade_class', 'historico_doenca_cardiaca_cat', 'Obesidade x Doença Cardíaca')
        
     # <<< NOVO GRÁFICO ADICIONADO AQUI >>>
-    with st.subheader("Obesidade x Colesterol Alto & Pressão Alta"):
+    st.subheader("Obesidade x Colesterol Alto & Pressão Alta")
 
     fig = px.bar(
         df_final,
@@ -293,14 +293,12 @@ with tab2:
         facet_col='Indicador',  # Cria um painel para cada indicador
         category_orders={'obesidade_class': LABELS_IMC}
     )
-    
+
     fig.update_traces(texttemplate='%{y:.1f}%', textposition='outside')
-    fig.update_layout(
-        yaxis_title="Percentual (%)",
-        xaxis_title="Classificação de Obesidade"
-    )
+    fig.update_layout(yaxis_title="Percentual (%)", xaxis_title="Classificação de Obesidade")
     
     st.plotly_chart(fig, use_container_width=True)
+
 
     with st.expander("Boxplots de Perfil Lipídico por Classe de Obesidade"):
         col_box1, col_box2, col_box3 = st.columns(3)
